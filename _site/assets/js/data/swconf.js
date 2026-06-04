3cea38e3cb18ba303b6c1ac5efdc15f05014c349
Alfred Abston <alfred.abston@proton.me>
2026-06-04 11:08:33 +0200

    Theme change...blue-neon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1780563565',resources: [
+    cacheName: 'chirpy-1780564034',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
