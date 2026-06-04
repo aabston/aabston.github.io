479652e85bbd8ebc89ba4fbc4aee45338d6eb57a
Alfred Abston <alfred.abston@proton.me>
2026-06-04 11:02:49 +0200

    Theme change...

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1780562414',resources: [
+    cacheName: 'chirpy-1780563565',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
